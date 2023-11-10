6bdd0f90939e39c1706fba39afa0d06fe17271d1
pooja2355 <94749004+pooja2355@users.noreply.github.com>
2023-11-10 15:28:52 +0530

    Add files via upload

---
+++ storage/framework/views/bb9e1cee16442c41fdd17dfa6dba26669388ce1b.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Page</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <style>
+        /* Custom CSS for the body */
+        body {
+            background-color: #f0f0f0; /* Change this color to your preferred background color */
+            padding-top: 20px; /* Adding space above the content */
+        }
+
+        /* Adjustments for the footer */
+        .site-footer {
+            background-color: #333; /* Change this color to your preferred footer background color */
+            color: #fff; /* Footer text color */
+            padding: 20px 0;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div style="clear:both" class="panel panel-default">
+        <div class="panel-footer"></div>
+    </div>
+    
+    <div class="site-footer">
+        <!-- Footer content -->
+        <p>© 2023 E-Shop. All rights reserved.</p>
+    </div>
+
+    <!-- Bootstrap and jQuery JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
+<?php /**PATH C:\Users\vivek\Downloads\eCom-en-laravel-master\eCom-en-laravel-master\resources\views/footer.blade.php ENDPATH**/ ?>
